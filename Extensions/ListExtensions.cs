18686f8d6b968558c1b6cd157c6db39bfe7b80be
Frelli <frellibb@gmail.com>
2015-01-31 22:36:11 +0100

    Shuffle bug fix.

--- Extensions/ListExtensions.cs
+++ Extensions/ListExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
 
 namespace GlowwormSelection.Extensions
 {
@@ -7,16 +9,27 @@ namespace GlowwormSelection.Extensions
     {
         public static void Shuffle<T>(this IList<T> list)
         {
-            Random rng = new Random();
             int n = list.Count;
             while (n > 1)
             {
                 n--;
-                int k = rng.Next(n + 1);
+                int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
                 T value = list[k];
                 list[k] = list[n];
                 list[n] = value;
             }
         }
     }
+
+    public static class ThreadSafeRandom
+    {
+        [ThreadStatic]
+        private static Random Local;
+
+        public static Random ThisThreadsRandom
+        {
+            get { return Local ?? (Local = new Random(unchecked(Environment.TickCount * 31 + Thread.CurrentThread.ManagedThreadId))); }
+        }
+    }
+
 }
