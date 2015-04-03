d39d0066df519e0a93f2493db5fb4a7f81bd6e92
Frelli <frellibb@gmail.com>
2015-04-03 02:34:19 +0200

    Ordered Crossover

---
+++ GeneticAlgorithm/Crossover/Crossover.cs
@@ -0,0 +1,61 @@
+﻿using GlowwormSelection.Extensions;
+using GlowwormSelection.GeneticAlgorithm.TSP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlowwormSelection.GeneticAlgorithm.Crossover
+{
+    class Crossover
+    {
+        public static Chromosome MakeChild(Chromosome parent1, Chromosome parent2)
+        {
+            //select subset of cities from p1
+
+            //select 2 points for the start and end of the subset
+            int size = parent1.cities.Count;
+
+            int point1 = ThreadSafeRandom.CurrentThreadRandom.Next(0, size - 1);
+            int point2 = ThreadSafeRandom.CurrentThreadRandom.Next(0, size);
+
+            int subsetStart = Math.Min(point1, point2);
+            int subsetEnd = Math.Max(point1, point2);
+
+            List<City> child = new List<City>();
+
+            // select the subset from the first parent
+            List<City> subset = parent1.cities.GetRange(subsetStart, subsetEnd - subsetStart);
+
+            // order the subset so that they have the same order as in p2
+            Queue<City> orderedSubset = new Queue<City>();
+
+            for (int i = 0; i < size; i++)
+            {
+                if (subset.Contains(parent2.cities[i]))
+                {
+                    orderedSubset.Enqueue(parent2.cities[i]);
+                }
+            }
+
+            // create the child
+            for (int i = 0; i < size; i++)
+            {
+                City curr = parent1.cities[i];
+                if (subset.Contains(curr))
+                {
+                    // if the current city is in the subset, get the ordered crossover city from the orderedsubset
+                    child.Add(orderedSubset.Dequeue());
+                }
+                else
+                {
+                    // else just add the city
+                    child.Add(curr);
+                }
+            }
+
+            return new Chromosome(child);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using GlowwormSelection.GeneticAlgorithm.SelectionSchemes;
+﻿using GlowwormSelection.GeneticAlgorithm.Crossover;
+using GlowwormSelection.GeneticAlgorithm.SelectionSchemes;
 using GlowwormSelection.GeneticAlgorithm.TSP;
 using System;
 
@@ -8,17 +9,55 @@ namespace GlowwormSelection
     {
         static void Main(string[] args)
         {
-            Population p = new Population();
-            p.GenerateCityData(1000);
-            p.GenerateInitialPopulation(100);
 
-            var gso = new GlowwormSwarmSelection().Select(p.chromosomes, 10);
-            p.ResetChromosomeFitness();
-            var tourn = new TournamentSelection().Select(p.chromosomes, 10);
-            p.ResetChromosomeFitness();
-            var trunc = new TruncateSelection().Select(p.chromosomes, 10);
-            p.ResetChromosomeFitness();
-            var roul = new RouletteWheelSelection().Select(p.chromosomes, 10);
+
+            Population p = new Population();
+            p.GenerateCityData(10);
+            p.GenerateInitialPopulation(10);
+
+            var parent1 = p.chromosomes[0];
+            var parent2 = p.chromosomes[1];
+
+            var child1 = Crossover.MakeChild(parent1, parent2);
+            var child2 = Crossover.MakeChild(parent2, parent1);
+
+            Console.Write("Parent 1:\t");
+            foreach (var item in parent1.cities)
+            {
+                Console.Write(item.Name + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Parent 2:\t");
+            foreach (var item in parent2.cities)
+            {
+                Console.Write(item.Name + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Child 1:\t");
+            foreach (var item in child1.cities)
+            {
+                Console.Write(item.Name + " ");
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Child 2:\t");
+            foreach (var item in child2.cities)
+            {
+                Console.Write(item.Name + " ");
+            }
+
+            //var gso = new GlowwormSwarmSelection().Select(p.chromosomes, 10);
+            //p.ResetChromosomeFitness();
+            //var tourn = new TournamentSelection().Select(p.chromosomes, 10);
+            //p.ResetChromosomeFitness();
+            //var trunc = new TruncateSelection().Select(p.chromosomes, 10);
+            //p.ResetChromosomeFitness();
+            //var roul = new RouletteWheelSelection().Select(p.chromosomes, 10);
 
 
 
